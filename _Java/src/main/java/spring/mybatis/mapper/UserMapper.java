a05bbf185f7e501336919109d7e4bce31f93932e
wanghaojie <1624302283@qq.com>
2022-09-16 18:27:09 +0800

    Mybatis原理整合

---
+++ _Java/src/main/java/spring/mybatis/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package spring.mybatis.mapper;
+
+import spring.mybatis.model.User;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @ClassName: UserMapper
+ * @Author: WHJ
+ * @Date: 2022/9/16 18:10
+ * @Version: 6.0.18.0
+ */
+public class UserMapper {
+	List<User> selectList;
+}
